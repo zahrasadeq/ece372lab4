284223807a90a7106aedf3225cac3cbdc2bab4f1
Andrew Enriquez <andrewenriquez@email.arizona.edu>
2018-10-22 15:27:54 -0700

    PWM

--- LAB_4/src/PWM.cpp
+++ LAB_4/src/PWM.cpp
@@ -4,5 +4,9 @@
 void initPWMTimer1() {
     DDRB |= (1 << DDB5); //pin 11 on dev board
 
-    //T
+    TCCR1A |= (1 << COM1A1) | (1 << WGM11) | (1 << WGM10);
+    TCCR1B |= (1 << WGM12) | (1 << CS10);
+
+
+    OCR1A = 255;
 }
---
+++ LAB_4/src/PWM.h
@@ -0,0 +1,9 @@
+#ifndef PWM_H
+#define PWM_H
+
+#include <avr/io.h>
+
+void initPWMTimer1();
+//void delayUs(unsigned int delay);
+
+#endif
