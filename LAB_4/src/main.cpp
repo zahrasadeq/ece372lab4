cd754335d5e8a357893273f5d087356f636ec4de
Rachael Seedenburg <rtseeden@email.arizona.edu>
2018-10-22 14:44:17 -0700

    lab 4

---
+++ LAB_4/src/main.cpp
@@ -0,0 +1,23 @@
+// Author:
+// Net ID:
+// Date:
+// Assignment:
+//
+// Description: This file contains a programmatic overall description of the
+// program. It should never contain assignments to special function registers
+// for the exception key one-line code such as checking the state of the pin.
+//
+// Requirements:
+//----------------------------------------------------------------------//
+
+#include <avr/io.h>
+
+
+int main(){
+
+  while(1){
+  
+  }
+
+  return 0;
+}
