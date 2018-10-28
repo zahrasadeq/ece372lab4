20e59f839c25f376ab2bac6fd830fe430a1da8e4
Rachael Seedenburg <rtseeden@email.arizona.edu>
2018-10-28 15:44:35 -0700

    added switch

---
+++ LAB_4/src/switch.h
@@ -0,0 +1,14 @@
+/// Author:        Rachael Seedenburg, 
+// Net ID:         rtseeden, 
+// Date:           10/28/2018
+// Assignment:     Lab 4
+//------------------------------------------------------------------//
+// Description: Function prototypes for switch.cpp
+//----------------------------------------------------------------------//
+
+#ifndef SWITCH_H
+#define SWITCH_H
+
+void initSwitchPB3();
+
+#endif
